bd417eeb083f6f5df733bc80665e9cea2045da3f
Rob Evans <coolbloke1324@gmail.com>
2012-06-22 04:03:49 +0100

    First commit

---
+++ canvasDataEncoder.js
@@ -0,0 +1,75 @@
+function encode(canvas, x, y, maxX, data) {
+	"use strict";
+	// Stringify the data
+	var stringData = JSON.stringify(data),
+		ctx = canvas.getContext('2d'),
+		quartet,
+		quartets = [],
+		currentX = 0, currentY = 0,
+		i;
+
+	console.log('Encoding string: ' + stringData);
+
+	// Make sure the string has a length divisible by 3,
+	// or add padding
+	while (stringData.length % 3 > 0) {
+		stringData += ' ';
+	}
+
+	// Loop the string and generate quartet pixel data
+	for (i = 0; i < stringData.length; i += 3) {
+		quartet = [
+			stringData.charCodeAt(i),
+			stringData.charCodeAt(i + 1),
+			stringData.charCodeAt(i + 2),
+			255 // The last value is always 255 to represent full alpha
+		];
+		console.log('Encoded "' + stringData[i] + '" with data ' + quartet[0]);
+		console.log('Encoded "' + stringData[i + 1] + '" with data ' + quartet[1]);
+		console.log('Encoded "' + stringData[i + 2] + '" with data ' + quartet[2]);
+
+		quartets.push(quartet);
+	}
+
+	// Add the terminal quartet
+	quartets.push([3, 2, 1, 255]);
+
+	// Loop the quartets and paint them!
+	for (i = 0; i < quartets.length; i++) {
+		quartet = quartets[i];
+		console.log('Painting:', quartet[0] + ', ' + quartet[1] + ', ' + quartet[2] + ', 255');
+		ctx.fillStyle = 'rgba(' + quartet[0] + ', ' + quartet[1] + ', ' + quartet[2] + ', 255)';
+		ctx.fillRect(x + currentX, y + currentY, 1, 1);
+
+		currentX++;
+		if (currentX >= maxX) { currentX = 0; currentY++; }
+	}
+}
+
+function decode(canvas, x, y, maxX) {
+	"use strict";
+	var ctx = canvas.getContext('2d'),
+		imageData = ctx.getImageData(x, y, maxX, canvas.height).data,
+		run = true,
+		quadCode,
+		i = 0,
+		jsonString = '';
+
+	while (run) {
+		quadCode = String(imageData[i]) + ' ' + String(imageData[i + 1]) + ' ' + String(imageData[i + 2]);
+		if (quadCode === '3 2 1') {
+			// We have scanned the terminal code
+			// so exit the loop
+			run = false;
+			return JSON.parse(jsonString);
+		} else {
+			jsonString += String.fromCharCode(imageData[i]) + String.fromCharCode(imageData[i + 1]) + String.fromCharCode(imageData[i + 2]);
+		}
+		i += 4;
+
+		if (i > imageData.length) {
+			run = false;
+			console.log('Image JSON Decode Error!');
+		}
+	}
+}
